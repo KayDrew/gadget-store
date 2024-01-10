239e1dbfd1863b103c5858b9932d9c3514384bdc
KayDrew <kdtshehla@gmail.com>
2024-01-10 05:08:56 -0800

    added introduction

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,12 @@ export default function appRoutes(){
 
     async function home(req,res){
 
-        res.render("index");
+        let popularProducts=[{productName:"Samsung Headset Max Bass XR",
+    price:"R2799.00"}];
+
+        res.render("index",{popularProducts
+
+        });
 
     }
 
